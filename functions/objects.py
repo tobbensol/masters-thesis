00b1d48ef03e0c46c0b1b3733409e777960767eb
tobbensol <tobsol01@gmail.com>
2025-03-06 19:14:00 +0100

    i did in fact NOT mess everything up (persistence objects are now a thing, and added plot functions)

--- functions/objects.py
+++ functions/objects.py
@@ -5,21 +5,18 @@ from matplotlib import pyplot as plt
 
 
 class PersistenceData:
-    def __init__(self, persistence, paths, resolution=10, num_landscapes=3):
-        """
-        Encapsulates persistence data, paths, statistics, and landscape transformation.
-
-        :param persistence: List/array of persistence diagrams
-        :param paths: List/array of paths corresponding to the persistence diagrams
-        :param resolution: Resolution of the landscape transformation
-        :param num_landscapes: Number of landscapes to compute
-        """
+    def __init__(self, persistence, paths, type, resolution=10, num_landscapes=3):
         self.persistence = persistence
         self.paths = paths
         self.stats = self.compute_persistence_stats()
-        self.landscapes = self.compute_landscapes(resolution, num_landscapes)
-        self.resolution = resolution
-        self.num_landscapes = num_landscapes
+        self.landscape_model = Landscape(resolution=resolution, num_landscapes=num_landscapes)
+        self.landscapes = self.compute_landscapes()
+        self.plot_text = {
+            "LL": ["Flight Path", "Longitude", "Latitude"],
+            "A" : ["Flight Altitude", "Timestep", "Altitude"],
+            "S" : ["Flight Speed", "Timestep", "Speed"],
+            "H" : ["Flight rotation", "Timestep", "Rotation(Radians)"],
+        }[type]
 
     def compute_persistence_stats(self):
         """Computes statistics for the persistence diagrams."""
@@ -40,40 +37,40 @@ class PersistenceData:
 
         return np.array(dataset)
 
-    def compute_landscapes(self, resolution, num_landscapes):
+    def compute_landscapes(self):
         """Computes landscape transformations for persistence diagrams."""
-        landscape_model = Landscape(resolution=resolution, num_landscapes=num_landscapes)
-        landscapes = [landscape_model.fit_transform([pers]) for pers in self.persistence]
+        landscapes = [self.landscape_model.fit_transform([pers]) for pers in self.persistence]
         landscapes = np.array(landscapes)
         return landscapes.reshape(landscapes.shape[0], landscapes.shape[2])
 
-    def plot_diagram(self, index=0):
-        """Plots the persistence diagram and landscape of a given index."""
-        fig, axs = plt.subplots(3, 1, figsize=(5, 15))
+    def plot_diagram(self, index, axs=None, add_landscape:bool=False):
+        if axs is None:
+            fig_count = 3 if add_landscape else 2
+            fig, axs = plt.subplots(fig_count, 1, figsize=(5, 5*fig_count))
 
         path = self.paths[index]
         pers = self.persistence[index]
         landscape = self.landscapes[index]
 
-        # Color points by index to create a gradient effect
         num_points = path.shape[0]
-        colors = np.linspace(0, 1, num_points)  # Create a gradient scale
+        colors = np.linspace(0, 1, num_points)
 
         scatter = axs[0].scatter(path[:, 0], path[:, 1], c=colors, cmap="plasma", edgecolors="none")
-        axs[0].set_title("Flight Path (Gradient by Time)")
-        axs[0].set_xlabel("Longitude")
-        axs[0].set_ylabel("Latitude")
+        axs[0].set_title(self.plot_text[0])
+        axs[0].set_xlabel(self.plot_text[1])
+        axs[0].set_ylabel(self.plot_text[2])
 
-        # Add colorbar to indicate progression
         cbar = plt.colorbar(scatter, ax=axs[0])
         cbar.set_label("Time Step")
 
         gudhi.persistence_graphical_tools.plot_persistence_diagram(pers, axes=axs[1])
         axs[1].set_title("Persistence Diagram")
 
-        for i in range(self.num_landscapes):
-            axs[2].plot(landscape[i * self.resolution:(i + 1) * self.resolution], label=f"Landscape {i + 1}")
+        if add_landscape:
+            num_landscapes = self.landscape_model.num_landscapes
+            resolution = self.landscape_model.resolution
+            for i in range(num_landscapes):
+                axs[2].plot(landscape[i * resolution:(i + 1) * resolution], label=f"Landscape {i + 1}")
 
-        axs[2].legend()
-        axs[2].set_title("Persistence Landscape")
-        plt.show()
+            axs[2].legend()
+            axs[2].set_title("Persistence Landscape")
